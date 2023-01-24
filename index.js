48d091d2e3231619647421d782add09f8b27a1de
Nikhil <nikhilchhillar29@gmail.com>
2023-01-24 12:07:05 +0530

    final commit

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const cont = document.querySelector(".con");
 
 kits.forEach(k => {
     const btn = document.createElement("button");
+    btn.classList.add("btn");
+    btn.style.backgroundImage = "url(images/" + k + ".png)";
+    btn.innerText = k;
     cont.appendChild(btn);
+    const audio = document.createElement("audio");
+    audio.src = "sounds/" + k + ".mp3";
+    cont.appendChild(audio);
+
+    btn.addEventListener("click", () => {
+        audio.play();
+    });
+    window.addEventListener("keydown", (e) => {
+        console.log(e);
+        if (e.k === k.slice(0, 1)) {
+            audio.play();
+        };
+    })
 
 })
